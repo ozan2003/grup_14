548bd22d79d25eff253bf4d2a1c32a9183d1328a
Ozan Malci <ozan41094@gmail.com>
2024-12-06 12:36:12 +0300

    read_line fonksiyonu eklendi

--- src/read_line.c
+++ src/read_line.c
@@ -1,50 +1,60 @@
-#include "../include/utils/split_whitespace.h"
+#include "../include/utils/read_line.h"
 
 #include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 /**
- * @brief String'i boşluklara göre ayırır ve döndürür.
- * @param line Ayırılacak string.
- * @return Boşluklara göre ayrılmış string dizisi.
+ * @brief Bir satır okur ve döndürür.
+ * @return Okunan satır.
  *
  * @warning Bu fonksiyon, bellek yönetimi için malloc ve realloc fonksiyonlarını
  * kullanır. Bu nedenle, kullanılan belleği serbest bırakmak için free
  * fonksiyonunu kullanmayı unutmayın.
  */
-
-char** split_whitespace(char* line)
+char* read_line(void)
 {
-    int    bufsize = TOKEN_BUFFER_SIZE, position = 0;
-    char** tokens = malloc((bufsize) * sizeof(char*));
-    char * token, **tokens_backup;
+    int   bufsize  = BUFFER_SIZE;
+    int   position = 0;
+    char* buffer   = malloc(sizeof(char) * bufsize);
+    char  ch;
 
-    if (!tokens)
+    if (!buffer)
     {
         fprintf(stderr, "allocation error\n");
         exit(EXIT_FAILURE);
     }
 
-    token = strtok(line, TOKEN_DELIMITERS);
-    while (token != NULL)
+    while (true)
     {
-        tokens[position] = token;
+        // Read a character
+        ch = getchar();
+
+        // If we hit EOF, replace it with a null character and return.
+        if (ch == EOF || ch == '\n')
+        {
+            buffer[position] = '\0';
+            return buffer;
+        }
+        else
+        {
+            buffer[position] = ch;
+        }
         position++;
 
+        // If we have exceeded the buffer, reallocate.
         if (position >= bufsize)
         {
-            bufsize += TOKEN_BUFFER_SIZE;
-            tokens_backup = tokens;
-            tokens        = realloc(tokens, bufsize * sizeof(char*));
-            if (!tokens)
+            bufsize += BUFFER_SIZE;
+            buffer = realloc(buffer, bufsize);
+            if (!buffer)
             {
-                free(tokens_backup);
                 fprintf(stderr, "allocation error\n");
                 exit(EXIT_FAILURE);
             }
         }
-
-        token = strtok(NULL, TOKEN_DELIMITERS);
     }
-    tokens[position] = NULL;
-    return tokens;
+
+    return buffer;
 }
