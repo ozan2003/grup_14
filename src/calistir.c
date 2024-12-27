dfb287891a84033643b75aaf3248effe6775b8a6
Ozan Malci <ozan41094@gmail.com>
2024-12-27 15:20:58 +0300

    fonksiyon deklerasyonları .h dosyasına taşındı

--- src/calistir.c
+++ src/calistir.c
@@ -14,31 +14,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
- /**
-  * @brief Tek bir komutçuğu çalıştırır.
-  * Gerekirse komut için girdi ve çıktı dosyaları yönlendirilir ve arka planda çalışma durumu düzenlenir.
-  * @param args Komut ve argüman dizisi.
-  * @param input_file Okunacak dosyanın ismi. NULL olduğunda standart girdi kullanılır.
-  * @param output_file Yazılacak dosyanın ismi. NULL olduğunda standart çıktı kullanılır.
-  * @param is_background Komutun arka planda çalışma durumu. 1 ise arka planda çalışır.
-  */
-void execute_single_command(char** args,
-    char* input_file,
-    char* output_file,
-    int    is_background);
-
-/**
- * @brief Pipe (boru hattı) kullanarak birden fazla komutun ardışık çalışmasını sağlar.
- * @param args Komut ve argüman dizisi.
- * @param input_file Zincirin ilk komutunun girdisini sağlayan dosya.
- * @param output_file Zincirin son komutunun çıktısını kaydedecek dosya.
- * @param is_background Zincirleme komutların arka planda çalışma durumu.
- */
-void execute_pipeline(char** args,
-    char* input_file,
-    char* output_file,
-    int    is_background);
-
 /**
  * @brief Kullanıcı tarafından girilen bir komut dizisini çalıştırır.
  * Komutlar tekli, pipe (boru hattı) veya girdi-çıktı yönlendirmeleriyle çalışabilir.
